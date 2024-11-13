1739cc9fa41ffeb28beb3ede0e7d79f8c2cddbf1
powerSeries <joseesotolongo@gmail.com>
2024-11-13 09:19:51 -0500

    Added requirements.txt

--- weberm/sunshineEmr/views.py
+++ weberm/sunshineEmr/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data = {"content": "Gfg is the best"}
+    return render(request, "index.html", data)
