ee55724c43d0a55cc311f046d542b122d3ee0936
crespi1998 <crespi1998@gmail.com>
2024-11-20 15:26:12 -0500

    Installed and Configured Bootstrap in Django
    Co-authored-by: Jose Sotolongo <joseesotolongo@gmail.com>

--- weberm/sunshineEmr/views.py
+++ weberm/sunshineEmr/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    data = {"content": "Gfg is the best"}
-    return render(request, "index.html", data)
+    #data = {"content": "Gfg is the best"}
+    return render(request, "index.html")
