9e3983e078101f0b16ba3d9c6118c4c90d570fa2
powerSeries <joseesotolongo@gmail.com>
2024-12-08 12:04:57 -0500

    Basic calender loaded into view

--- weberm/sunshineEmr/views.py
+++ weberm/sunshineEmr/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    #data = {"content": "Gfg is the best"}
     return render(request, "index.html")
+
+    
--- weberm/weberm/settings.py
+++ weberm/weberm/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_bootstrap5',
+    'fullcalendar',
+
     'sunshineEmr.apps.SunshineEmrConfig',
 ]
 
